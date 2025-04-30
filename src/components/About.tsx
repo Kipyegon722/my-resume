d324082fee6ee4ed0b3d070f73604543e9a54b5b
Victor Kipyegon <kipyegon722@gmail.com>
2025-04-30 21:00:31 +0300

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -54,7 +54,7 @@ const About: React.FC = () => {
           <h2 className="text-3xl md:text-4xl font-bold mb-4">About Me</h2>
           <div className="w-20 h-1 bg-purple-500 mx-auto mb-8"></div>
           <p className="max-w-3xl mx-auto text-gray-600 text-lg">
-          I got into web development out of pure curiosity—and quickly found myself hooked. 
+          I got into web development out of pure curiosity and quickly found myself hooked. 
           There's something incredibly satisfying about turning ideas into real, functional websites using the MERN stack. 
           I enjoy the challenge of solving problems through code and love crafting clean, responsive designs that feel intuitive to use. For me, it’s all about creating smooth experiences that people enjoy interacting with.
           </p>
